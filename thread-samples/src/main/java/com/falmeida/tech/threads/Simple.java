a6211294405e647001dc253ec6f759cbc47f15ec
Fabio Almeida <falmeida.tech@gmail.com>
2020-12-08 23:48:14 -0500

    Added semaphore sample

--- thread-samples/src/main/java/com/falmeida/tech/threads/Simple.java
+++ thread-samples/src/main/java/com/falmeida/tech/threads/Simple.java
@@ -1,2 +1,16 @@
-package com.falmeida.tech.threads;public class Simple {
+package com.falmeida.tech.threads;
+
+public class Simple implements Runnable{
+    public void run() {
+        System.out.println("Runnable Thread");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+        Simple simple = new Simple();
+        Thread thread = new Thread(simple);
+        thread.start();
+
+    }
+
 }
