88db5105d84c21b7528547c1b462da95a8859f7e
Fabio Almeida <falmeida.tech@gmail.com>
2020-12-10 23:18:38 -0500

    Added join thread sample

---
+++ thread-samples/src/main/java/com/falmeida/tech/threads/MyRunnable.java
@@ -0,0 +1,15 @@
+package com.falmeida.tech.threads;
+
+class MyRunnable implements Runnable{
+
+    public void run() {
+        System.out.println("Thread started:::"+Thread.currentThread().getName());
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Thread ended:::"+Thread.currentThread().getName());
+    }
+
+}
